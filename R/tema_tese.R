382e9b6bf5fa36f6e5a089dd4f0085d9fc254a6f
Fernando Meireles <fernando.meireles@ufrgs.br>
2018-04-10 08:52:49 -0300

    Atualizada tema ggplot

--- R/tema_tese.R
+++ R/tema_tese.R
@@ -4,21 +4,21 @@
 #'
 #' @param tam.fonte Font size
 #' @param fonte Font type
+#' @param rel.eixo Axis text size
 #'
 #'
 #' @import ggplot2
 #' @export
 
-tema_tese <- function (tam.fonte = 14, fonte = "sans") {
+tema_tese <- function (tam.fonte = 12, fonte = "sans", rel.eixo = 0.8) {
 
   (ggplot2::theme_bw(base_size = tam.fonte, base_family = fonte) +
-     ggplot2::theme(axis.text = ggplot2::element_text(colour = "black", size = ggplot2::rel(1)),
+     ggplot2::theme(axis.text = ggplot2::element_text(colour = "black", size = ggplot2::rel(rel.eixo)),
                     axis.title = ggplot2::element_text(colour = "black"),
                     axis.title.y = ggplot2::element_text(angle = 90, margin = ggplot2::margin(0, 15)),
                     axis.title.x = ggplot2::element_text(margin = ggplot2::margin(15)),
                     axis.line.x = ggplot2::element_line(color = "black"),
                     axis.line.y = ggplot2::element_line(color = "black"),
-                    axis.ticks = ggplot2::element_blank(),
                     legend.position = "bottom",
                     legend.key = ggplot2::element_blank(),
                     legend.text = ggplot2::element_text(size = tam.fonte),
@@ -28,5 +28,5 @@ tema_tese <- function (tam.fonte = 14, fonte = "sans") {
                     panel.border = ggplot2::element_blank(),
                     panel.background = ggplot2::element_blank(),
                     plot.background = ggplot2::element_blank(),
-                    plot.title = ggplot2::element_text(size = ggplot2::rel(1), vjust = 0, margin = ggplot2::margin(0, 0, 20, 0))))
+                    plot.title = ggplot2::element_text(size = ggplot2::rel(1.05), vjust = 0, margin = ggplot2::margin(0, 0, 20, 0))))
 }
